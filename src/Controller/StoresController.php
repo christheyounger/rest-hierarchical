69c4fca15dcc68fbd70506854357a1515d7a43af
Chris Young <chris@chris-young.com>
2019-01-28 09:57:49 +1100

    Add get, post, put, and delete actions

--- src/Controller/StoresController.php
+++ src/Controller/StoresController.php
@@ -5,8 +5,9 @@ namespace App\Controller;
 use App\Entity\Store;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations as REST;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class StoresController extends AbstractFOSRestController implements ClassResourceInterface
 {
@@ -22,5 +23,38 @@ class StoresController extends AbstractFOSRestController implements ClassResourc
   {
     $repo = $this->entityManager->getRepository(Store::class);
     return $repo->findAll();
+  }
+
+  public function getAction(Store $store): Store
+  {
+    return $store;
+  }
+
+  /**
+   * @REST\RequestParam(name="name", nullable=false)
+   */
+  public function postAction(Request $request): Store
+  {
+    $store = new Store();
+    $store->setName($request->get('name'));
+    $this->entityManager->persist($store);
+    $this->entityManager->flush();
+    return $store;
+  }
+
+  /**
+   * @REST\RequestParam(name="name", nullable=false)
+   */
+  public function putAction(Request $request, Store $store): Store
+  {
+    $store->setName($request->get('name'));
+    $this->entityManager->flush();
+    return $store;
+  }
+
+  public function deleteAction(Store $store): void
+  {
+    $this->entityManager->remove($store);
+    $this->entityManager->flush();
   }
 }
--- tests/unit/Controller/StoresControllerTest.php
+++ tests/unit/Controller/StoresControllerTest.php
@@ -7,6 +7,7 @@ use App\Entity\Store;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
 
 class StoresControllerTest extends TestCase
 {
@@ -19,13 +20,55 @@ class StoresControllerTest extends TestCase
     $this->controller = new StoresController($this->entityManager);
   }
 
-  public function testCgetAction()
+  public function testCgetAction(): void
   {
     $stores = [new Store()];
     $repo = $this->createMock(EntityRepository::class);
     $this->entityManager->method('GetRepository')->willReturn($repo);
     $repo->expects(static::once())->method('findAll')->willReturn($stores);
     $result = $this->controller->cgetAction();
-    $this->assertEquals($stores, $result);
+    $this->assertEquals($stores, $result, 'returns the stores');
+  }
+
+  public function testGetAction(): void
+  {
+    $store = new Store();
+    $this->assertEquals(
+      $store,
+      $this->controller->getAction($store),
+      'returns the store'
+    );
+  }
+
+  public function testPostAction(): void
+  {
+    $data = ['name' => 'new store'];
+    $request = new Request([], $data);
+    $this->entityManager->expects(static::once())->method('persist');
+    $this->entityManager->expects(static::once())->method('flush');
+    $result = $this->controller->postAction($request);
+    $this->assertInstanceOf(Store::class, $result, 'returns a store');
+    $this->assertEquals('new store', $result->getName(), 'name set correctly');
+  }
+
+  public function testPutAction(): void
+  {
+    $store = new Store();
+    $store->setName('old name');
+    $data = ['name' => 'new name'];
+    $request = new Request([], $data);
+    $this->entityManager->expects(static::once())->method('flush');
+    $result = $this->controller->putAction($request, $store);
+    $this->assertInstanceOf(Store::class, $result, 'returns a store');
+    $this->assertEquals($store, $result, 'returned same object');
+    $this->assertEquals('new name', $result->getName(), 'name set correctly');
+  }
+
+  public function testDeleteAction(): void
+  {
+    $store = new Store();
+    $this->entityManager->expects(static::once())->method('remove')->with($store);
+    $this->entityManager->expects(static::once())->method('flush');
+    $result = $this->controller->deleteAction($store);
   }
 }
